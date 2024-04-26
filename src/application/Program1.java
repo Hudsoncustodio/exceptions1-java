2fbe67b9dd946324bf44f356fe75c67bc4330db3
Hudson Ferreira <141644371+Hudsoncustodio@users.noreply.github.com>
2024-04-26 16:49:43 -0300

    Bad solution

--- src/application/Program1.java
+++ src/application/Program1.java
@@ -7,9 +7,8 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
-
 public class Program1 {
-
+	
 	public static void main(String[] args) throws ParseException{
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -35,18 +34,19 @@ public class Program1 {
 			System.out.print("Check out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates!");
-			}
-			else if(!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date!");
+			String error = reservation.updateDates(checkIn,  checkOut);
+			if(error != null) {
+				System.out.print("Error in resevation: " + error);
 			}
 			else {
-				Reservation reservation1 = new Reservation(number, checkIn, checkOut);
-				System.out.print("Resevation: " + reservation1);
+			System.out.println("Reservation: " + reservation);
 			}
-		}		
+		}
 		sc.close();
 	}
 }
+
+
+
+
+	
